9933c3104509935bdfaca3dd120382930f60d280
jeremy <jeyx.22@gmail.com>
2020-03-27 11:16:33 +0100

    add function render to draw on the screen

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-// new array for each generation
-function create_cells(rows, cols) {
+"use strict";
+function create_cells(dim) {
     // create a 2d array
-    var cells = new Array(rows);
-    for (var i = 0; i < cols; i++)
-        cells[i] = new Array(rows).fill(null);
+    let cells = new Array(dim);
+    for (let i = 0; i < dim; i++) {
+        cells[i] = new Array(dim).fill(null);
+    }
     return cells;
 }
 function init_cells(cells) {
     // initialize each cell with a random state (alive or dead)
-    return cells.map(function (x) { return x.map(function (i) { return i = Math.random() > 0.5 ? true : false; }); });
+    return cells.map(row => row.map(cell => (cell = Math.random() > 0.5)));
 }
-function update() {
+function update(cells) {
     // check every cell and its neighbours and create a new array of cells based on the rules
+    return cells.map((row, idx, arr) => { });
 }
-function render(cells) {
+function render(cells, canvas, scale) {
     // draw the array of cells on the screen
+    let ctx = canvas.getContext("2d");
+    let w = canvas.width;
+    let x = 0;
+    let y = 0;
+    let dim = (w / scale) * 2;
+    cells.map(row => row.map(cell => {
+        ctx.fillStyle = cell ? "white" : "black";
+        ctx.fillRect(x, y, dim, dim);
+        if (x + dim > w + canvas.offsetLeft) {
+            x = 0;
+            y += dim;
+        }
+        else
+            x += dim;
+    }));
 }
-// render(update());
+let scale = 50;
+render(init_cells(create_cells(scale)), document.getElementById("canvas"), scale);
--- script.ts
+++ script.ts
@@ -1,21 +1,47 @@
-function create_cells(rows: number, cols: number) {
+function create_cells(dim: number): any[][] {
 	// create a 2d array
-	let cells = new Array(rows);
-	for (let i = 0; i < cols; i++)
-		cells[i] = new Array(rows).fill(null);
+	let cells = new Array(dim);
+	for (let i = 0; i < dim; i++) {
+		cells[i] = new Array(dim).fill(null);
+	}
 
 	return cells;
 }
 
-function init_cells(cells: any[][]) {
+function init_cells(cells: any[][]): boolean[][] {
 	// initialize each cell with a random state (alive or dead)
-	return cells.map(x => x.map(i => i = Math.random() > 0.5 ? true : false));
+	return cells.map(row => row.map(cell => (cell = Math.random() > 0.5)));
 }
 
-function update() {
+function update(cells: boolean[][]) {
 	// check every cell and its neighbours and create a new array of cells based on the rules
+	return cells.map((row, idx, arr) => {});
 }
 
-function render(cells: any[][]) {
+function render(cells: boolean[][], canvas: HTMLCanvasElement, scale: number) {
 	// draw the array of cells on the screen
+	let ctx = canvas.getContext("2d")!;
+	let w = canvas.width;
+	let x = 0;
+	let y = 0;
+	let dim = (w / scale) * 2;
+
+	cells.map(row =>
+		row.map(cell => {
+			ctx.fillStyle = cell ? "white" : "black";
+			ctx.fillRect(x, y, dim, dim);
+
+			if (x + dim > w + canvas.offsetLeft) {
+				x = 0;
+				y += dim;
+			} else x += dim;
+		})
+	);
 }
+
+let scale = 50;
+render(
+	init_cells(create_cells(scale)),
+	<HTMLCanvasElement>document.getElementById("canvas"),
+	scale
+);
